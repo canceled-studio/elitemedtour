f7c5f24c6b9d4b86071caba361030a4f38a443ec
V <wadedevelop@gmail.com>
2023-06-26 05:38:14 +0000

    added "main" dynamic path

--- src/pages/main/[slug].jsx
+++ src/pages/main/[slug].jsx
@@ -1,3 +1,25 @@
-export default function News() {
-  return <h1>Hello</h1>;
+import { Body, Header, MDXBody, Section } from "@/components/section";
+import { getAllContentByType, getSingleContent } from "@/lib/api/content";
+
+export default function Main({ page }) {
+  return (
+    <Section>
+      <Header>{page.frontmatter.title}</Header>
+      <MDXBody>{page.content}</MDXBody>
+    </Section>
+  );
 }
+
+export const getStaticProps = async ({ params }) => {
+  const fullContentSlug = `main/${params.slug}`;
+  const page = await getSingleContent(fullContentSlug);
+
+  return { props: { page } };
+};
+
+export const getStaticPaths = (params) => {
+  const allContent = getAllContentByType("main");
+  const allSlugs = allContent.map((item) => item.slug);
+
+  return { paths: allSlugs, fallback: false };
+};
