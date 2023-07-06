c35a11d45707c4c651a929a51232f1f4fd110582
V <wadedevelop@gmail.com>
2023-07-06 03:07:11 +0000

    add menu "selected" state

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,19 +1,22 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 import { Header } from "@/components/section";
 import menuItems from "@/_content/menu.json";
+import { useRouter } from "next/router";
 
-export function MenuItem({ title, link }) {
+export function MenuItem({ title, link, selected }) {
   const common =
     "px-1 py-2 text-white no-underline flex justify-center text-sm";
   const hover = "hover:bg-yellow-500 hover:shadow-yellow-200";
-  const selected = "bg-gray-400 shadow-gray-300";
 
   return (
     <li>
       <Link
         href={link}
-        className={`${common} shadow-[inset_4px_4px_8px_0px,inset_-0_-1px_2px_2px] shadow-sky-400 rounded-2xl ${hover}`}
+        className={`${common} ${
+          selected ? "bg-gray-400 shadow-gray-300" : ""
+        } shadow-[inset_4px_4px_8px_0px,inset_-0_-1px_2px_2px] shadow-sky-400 rounded-2xl ${hover}`}
       >
         {title}
       </Link>
@@ -22,11 +25,18 @@ export function MenuItem({ title, link }) {
 }
 
 export function MenuItemList() {
+  const { asPath } = useRouter();
+
   return (
     <nav className="py-4 px-2 section-body">
       <ul className="text-center flex flex-col gap-2">
         {menuItems.map(({ title, link }) => (
-          <MenuItem key={link} title={title} link={link} />
+          <MenuItem
+            key={link}
+            selected={asPath.startsWith(link)}
+            title={title}
+            link={link}
+          />
         ))}
       </ul>
     </nav>
