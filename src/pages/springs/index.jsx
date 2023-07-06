369238b94059d393b94fe103094eb8727a5755e9
V <wadedevelop@gmail.com>
2023-07-06 05:07:19 +0000

    springs/index page implementation

---
+++ src/pages/springs/index.jsx
@@ -0,0 +1,27 @@
+import { MDXRemote } from "next-mdx-remote";
+import { Section, Header, Body } from "@/components/section";
+import { getAllContentByType, getSingleContent } from "@/lib/api/content";
+import { ContentGrid } from "@/components/content-grid";
+
+export default function Springs({ page, items }) {
+  return (
+    <Section>
+      <Header>{page.frontmatter.title}</Header>
+      <Body>
+        <MDXRemote {...page.content} />
+      </Body>
+      <Body>
+        <ContentGrid items={items} />
+      </Body>
+    </Section>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      page: await getSingleContent("sections/springs/index"),
+      items: getAllContentByType("springs"),
+    },
+  };
+}
