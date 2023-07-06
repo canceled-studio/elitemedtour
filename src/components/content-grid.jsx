ce213460084d6635262a43302b45cf31ff0f88b5
V <wadedevelop@gmail.com>
2023-07-06 05:07:00 +0000

    content-grid: implementation

---
+++ src/components/content-grid.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export function ContentGridItem({ title, slug, image }) {
+  return (
+    <div className="flex flex-col gap-2">
+      <Link className="no-underline" href={slug}>
+        <h4 className="text-yellow-400">{title}</h4>
+        <hr className="my-2" />
+        <Image
+          className="object-cover w-[250px] h-[125px]"
+          width="250"
+          height="200"
+          alt={title}
+          src={image}
+        />
+      </Link>
+    </div>
+  );
+}
+
+export function ContentGrid({ items }) {
+  return (
+    <div className="grid grid-cols-3 gap-5">
+      {items.map(({ slug, frontmatter }) => (
+        <ContentGridItem key={slug} slug={slug} {...frontmatter} />
+      ))}
+    </div>
+  );
+}
