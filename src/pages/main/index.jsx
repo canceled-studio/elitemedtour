0f9ff78426495f05f332aeabc9676b342daea168
V <wadedevelop@gmail.com>
2023-07-11 02:25:17 +0000

    fix: move "main" page into _content/section

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,11 +1,11 @@
-import { getAllContentByType } from "@/lib/api/content";
+import { getAllContentByType, getSingleContent } from "@/lib/api/content";
 import { ContentList } from "@/components/content-list";
 import { Body, Header, Section } from "@/components/section";
 
-export default function Main({ items }) {
+export default function Main({ page, items }) {
   return (
     <Section>
-      <Header>Главная</Header>
+      <Header>{page.frontmatter.title}</Header>
       <Body>
         <ContentList items={items} />
       </Body>
@@ -13,6 +13,11 @@ export default function Main({ items }) {
   );
 }
 
-export const getStaticProps = () => {
-  return { props: { items: getAllContentByType("main") } };
+export const getStaticProps = async () => {
+  return {
+    props: {
+      page: await getSingleContent("sections/main/index"),
+      items: getAllContentByType("main"),
+    },
+  };
 };
