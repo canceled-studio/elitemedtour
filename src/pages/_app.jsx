15334948519334ac63842c3fb58ea7f4ecb26701
V <wadedevelop@gmail.com>
2023-07-19 05:11:55 +0000

    add page title

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,15 @@
-import { MDXProvider } from "@mdx-js/react";
+import Head from "next/head";
 import Layout from "@/components/layout";
 import "@/styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
+  const title = `${pageProps?.page?.frontmatter?.title} | Elitemedtour`;
+
   return (
     <Layout>
+      <Head>
+        <title>{title}</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
